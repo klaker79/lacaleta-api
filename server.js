9b1e0216216ea8c482f5d604845f1024543ebbaa
Iker Fernandez <ikerameas@gmail.com>
2025-12-11 01:28:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors({
-  
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.json());
 app.options('*', cors());
