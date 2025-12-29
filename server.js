9ab724f51727395b63e217f93326008d8101831b
Iker Fernandez Caballero <ikerfernandezcaballero@Ikers-MacBook-Air.local>
2025-12-29 22:58:18 +0100

    fix: Add more localhost ports to CORS

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ const DEFAULT_ORIGINS = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004',
+    'http://localhost:3005',
+    'http://localhost:3006',
+    'http://localhost:3007',
     'http://localhost:8080'
 ];
 const ENV_ORIGINS = process.env.ALLOWED_ORIGINS
